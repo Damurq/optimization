19ae5b1bf5aa824bb5647f67343f845cb3cdf9fa
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2022-12-10 13:13:12 -0400

    actualización del tamaño de las tablas

--- src/cauchy.m
+++ src/cauchy.m
@@ -59,5 +59,14 @@ function point = cauchy(fx, initial, tolerance, maxIter, bl,xx,yy)
     f = figure;
     t = uitable('ColumnName', {'iteración', 'x1', 'x2', 'alfa', 'd1', 'd2'});
     drawnow;
+    % Establece las unidades de la tabla como porcentajes
+    set(t, 'Units', 'normalized');
+
+    % Define el nuevo tamaño de la tabla en porcentajes
+    newPosition = [0 0 1 1];
+
+    % Establece el nuevo tamaño de la tabla
+    set(t, 'Position', newPosition);
     set(t, 'Data', table);
+    
 end
